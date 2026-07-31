213504eaa1d0609e490c32dcf8872aecc9f658f3
vinodnaikr <142738355+vinodnaikr@users.noreply.github.com>
2026-07-31 10:04:29 +0530

    Time: 111 ms (47.83%), Space: 19.9 MB (93.65%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minimumPushes(self, word: str) -> int:
+        freq = Counter(word)
+
+        frequencies = sorted(freq.values(),reverse=True)
+
+        total = 0
+
+        for i,f in enumerate(frequencies):
+            position = i//8 + 1
+            total+=f*position
+
+        return total
+        
